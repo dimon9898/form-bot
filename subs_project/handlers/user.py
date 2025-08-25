b156e99e3b9bc6b99e2cf4848f17d017c9cd4a30
Dilshod <dilshod.khaidkulov@gmail.com>
2025-08-25 18:18:03 +0300

    finish bot

--- subs_project/handlers/user.py
+++ subs_project/handlers/user.py
@@ -1,4 +1,5 @@
-from aiogram.types import Message, CallbackQuery
+import json
+from aiogram.types import Message, CallbackQuery, LabeledPrice
 from aiogram.filters import CommandStart
 from aiogram import Router, F
 from keyboards.user_kb import user_main
@@ -12,3 +13,28 @@ async def cmd_start(message: Message):
     await rq.set_user(user_id)
     await message.answer('Добро пожаловать!\n\n'
                          'Сначала оформите подписку:', reply_markup=await user_main())
+
+@user.message(F.web_app_data)
+async def get_webapp(message: Message):
+    text = message.web_app_data.data
+    data = json.loads(text)
+    fullname = str(data['fullname'])
+    email = str(data['email'])
+    subs_id = int(data['tarif'])
+    subs = await rq.get_subscription(subs_id)
+    await message.answer(f'Имя: {fullname}\n'
+                         f'Почта: {email}\n'
+                         f'Тариф: {subs.name}')
+    price = [LabeledPrice(label='Оплата за подписку', amount=subs.price * 100)]
+    await message.bot.send_invoice(
+        chat_id=message.from_user.id,
+        title=subs.name,
+        description=f'{subs.period} дней',
+        payload=f'subscription_{subs.id}_{message.from_user.id}_{fullname}_{email}',
+        currency='RUB',
+        provider_token='1744374395:TEST:b9879982909d7c1bd39c',
+        prices=price,
+        start_parameter='create_order'
+    )   
+    
+    
--- subs_project/keyboards/user_kb.py
+++ subs_project/keyboards/user_kb.py
@@ -1,6 +1,7 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import WebAppInfo
 
 async def user_main():
     kb = ReplyKeyboardBuilder()
-    kb.button(text='Открыть')
+    kb.button(text='Открыть', web_app=WebAppInfo(url='https://form-app-puce.vercel.app/'))
     return kb.as_markup(resize_keyboard=True)
